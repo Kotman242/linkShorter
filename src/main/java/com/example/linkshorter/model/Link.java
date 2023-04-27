14957ed2874472a401779ce8953f3a01eabf8fb7
Andrew Zakharenko <andreyzaharenko81@gmail.com>
2023-04-27 10:56:40 +0300

    refactoring

--- src/main/java/com/example/linkshorter/model/Link.java
+++ src/main/java/com/example/linkshorter/model/Link.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -12,17 +13,22 @@ import java.util.Date;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table (name = "links")
+@Table(name = "links")
 @Builder
 public class Link {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    @Column(unique = true)
+    @Column(unique = true,
+            nullable = false)
     private String shortLink;
-    @Column(unique = true)
+    @Column(unique = true,
+            nullable = false)
     private String longLink;
 
     @Temporal(TemporalType.DATE)
-    private Date date = new Date();
+    @Column(name = "create_date",
+            nullable = false)
+    @ColumnDefault("now()")
+    private Date date;
 }
