7c13cb330d7b303da78d7fdfae066341e22a988f
Andrew Zakharenko <andreyzaharenko81@gmail.com>
2023-05-01 12:56:14 +0300

    encoder added

---
+++ src/main/java/com/example/linkshorter/service/PeopleRegistrationService.java
@@ -0,0 +1,25 @@
+package com.example.linkshorter.service;
+
+import com.example.linkshorter.model.Person;
+import com.example.linkshorter.repository.PersonRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@AllArgsConstructor
+public class PeopleRegistrationService implements RegistrationService{
+
+    private final PersonRepository repository;
+    private final PasswordEncoder encoder;
+
+    @Override
+    @Transactional
+    public void register(Person person) {
+        person.setPassword(encoder.encode(person.getPassword()));
+
+        repository.save(person);
+    }
+}
